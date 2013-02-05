b35feea1ca304e587d256faea06462fb5d7c6026
Matěj Laitl <matej@laitl.cz>
2013-02-05 23:28:01 +0100

    Improve and fix bench test

--- ceygen/tests/bench.pyx
+++ ceygen/tests/bench.pyx
@@ -12,39 +12,59 @@ from support import CeygenTestCase, benchmark
 cimport ceygen.core as c
 
 
+class timeit:
+    """Simple context manager to time interations of the block inside"""
+
+    def __init__(self, name, align, iterations, cost):
+        self.iterations, self.cost = iterations, cost
+        self.name = name.rjust(align)
+
+    def __enter__(self):
+        self.elapsed = time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        self.elapsed = time() - self.elapsed
+        percall = self.elapsed/self.iterations
+        print "{0}: {1:.2e}s per call, {2:.3f}s total, {3:5.2f} GFLOPS".format(self.name,
+              percall, self.elapsed, self.cost/percall/10.**9)
+        return False  # let the exceptions fall through
+
+
 class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_dot_mm(self):
         print
         cdef int iterations
-        cdef double[:, :] x
-        cdef double[:, :] y
-        cdef double[:, :] out
+        cdef double[:, :] x, x_nocontig, y, out
 
         for size in (2, 3, 4, 6, 8, 12, 16, 24, 32, 48, 64, 100):
             x_np = np.random.rand(size, size)
+            #x_nocontig_np = np.random.rand(size, size, 2)[:, :, 0]
             y_np = np.random.rand(size, size)
             out_np = np.empty((size, size))
             x = x_np
+            #x_nocontig = x_nocontig_np
             y = y_np
             out = out_np
 
             cost = 2. * size**3.
             iterations = min(2. * 10.**9. / cost, 1000000)
             print "size: {0}x{0}, iterations: {1}".format(size, iterations)
+            align = 8
 
-            elapsed = time()
-            for i in range(iterations):
-                np_dot(x_np, y_np, out_np)
-            elapsed = time() - elapsed
-            print "   numpy: {0:.2e}s per call, {1:.3f}s total, {2:5.2f} GFLOPS".format(elapsed/iterations, elapsed, cost*iterations/elapsed/10.**9)
-            out_copy = out.copy()
+            with timeit("numpy", align, iterations, cost):
+                for i in range(iterations):
+                    np_dot(x_np, y_np, out_np)
+            with timeit("ceygen", align, iterations, cost):
+                for i in range(iterations):
+                    c.dot_mm(x, y, out)
 
-            elapsed = time()
-            for i in range(iterations):
-                c.dot_mm(x, y, out)
-            elapsed = time() - elapsed
-            print "   numpy: {0:.2e}s per call, {1:.3f}s total, {2:5.2f} GFLOPS".format(elapsed/iterations, elapsed, cost*iterations/elapsed/10.**9)
-
-            self.assertApproxEqual(out, out_copy)
+            #iterations /= 3
+            #with timeit("numpy-nocontig", align, iterations, cost):
+                #for i in range(iterations):
+                    #np_dot(x_nocontig_np, y_np, out_np)
+            #with timeit("ceygen-nocontig", align, iterations, cost):
+                #for i in range(iterations):
+                    #c.dot_mm(x_nocontig, y, out)
