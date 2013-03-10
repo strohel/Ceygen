0e49087c4de4e1cc21e782586eb374a6931418b5
Matěj Laitl <matej@laitl.cz>
2013-03-10 15:59:29 +0100

    setup.py: set version to 0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
 
     # meta-data; see http://docs.python.org/distutils/setupscript.html#additional-meta-data
     name='Ceygen',
-    version="0.2-pre",
+    version="0.2",
     author='Matěj Laitl',
     author_email='matej@laitl.cz',
     maintainer='Matěj Laitl',
